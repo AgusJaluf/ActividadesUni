b5ee93e304f2d4d8086546c2975dae155e13f9c6
AgusJaluf <agsjaluf@hotmail.com>
2025-12-15 23:17:44 -0300

    Crear contador.py

---
+++ contador.py
@@ -0,0 +1,10 @@
+contador = 0
+while (True):
+   numero = input()
+   if numero[0] == numero[2]:
+      True
+      contador = contador+1
+   else:
+       False
+       print (contador)
+       break
